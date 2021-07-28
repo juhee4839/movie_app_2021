f85811a8f57ad78576f2a4bf3ece17a6473cdfbe
juhee <juheekim@Juui-MacBookPro.local>
2021-07-28 13:12:52 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,54 +1,26 @@
 import React from "react";
+import propTypes from "prop-types";
 
-const foodLike = [
-{
-  id: 1,
-  name: "backdosan",
-  image: 
-  "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1040&q=80",
-  rating: 5
-},
-{
-  id: 2,
-  name: "insideout",
-  image:
-  "https://images.unsplash.com/photo-1542204165-65bf26472b9b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2767&q=80",
-  rating: 4.6
-},
-{
-  id: 3,
-  name: "incredable",
-  image:
-  "https://images.unsplash.com/photo-1543536448-1e76fc2795bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2824&q=80",
-  rating: 4.9
+class App extends React.Component {
+  state ={
+    count: 0
+  };
 
-},
-{
-  id: 4,
-  name:"zotopia",
-  image:
-  "https://images.unsplash.com/photo-1527606078995-42486f991014?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2855&q=80",
-  rating: 5
+  add = () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
+  };
+ 
+  render(){
+    return(
+      <div>
+        <h1> The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
-];
-
-function Food({ name, picture }){
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <img src={picture} alt={name}/>
-    </div>
-  );
-}
-
-function App() {
-  return (
-    <div>
-      {foodLike.map(dish => (
-      <Food key={dish.id} name={dish.name} picture={dish.image}/> ))}
-    </div>
-  );
-}
-
-
 export default App; 
