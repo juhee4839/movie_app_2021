f79062204990164ad13381b742bd9b5ad032f75e
juhee <79627080+juhee4839@users.noreply.github.com>
2021-08-01 14:23:55 +0900

    poster

--- README.md
+++ README.md
@@ -2,7 +2,4 @@
 
 React JS Fundamentals Course (2021 Update!!)
 
-//////////
-
-////////////////
 ////test
--- src/App.js
+++ src/App.js
@@ -11,10 +11,12 @@ class App extends React.Component {
   getMovies = async () => {
     const {
       data: {
-        data : { movies }
+        data: { movies }
       }
-    } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
-      this.setState({movies});
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
   };
 
   componentDidMount (){
@@ -22,19 +24,23 @@ class App extends React.Component {
   }
  
   render() {
-    const { isLoading , movies} = this.state;
-    return <div>{isLoading ? "Loading..." : movies.map(movie => {
-      console.log(movie);
-      return(
-        <Movie
-          id={movies.id}
-          year={movies.year}
-          title={movies.title}
-          summary={movies.summary}
-          poster={movies.}
-          />
-      )
-    })}</div>
-  }
-}
+     const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+              ))}
+              </div>
+            );
+          }
+        }
 export default App; 
